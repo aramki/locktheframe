3cd2da10d837e784f529d068600d2174926e804f
Ramakrishnan A <aramkris@gmail.com>
2019-01-22 17:49:20 +0530

    Simple Web Server

--- main.go
+++ main.go
@@ -2,8 +2,15 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
+func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1>First print</h1>")
+}
+
 func main() {
-	fmt.Println("Hello! Lock the Frame")
+	//fmt.Println("Hello! Lock the Frame")
+	http.HandleFunc("/", handlerFunc)
+	http.ListenAndServe("localhost:8000", nil)
 }
