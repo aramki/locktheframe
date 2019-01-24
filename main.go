bcbfd37256b3f32ef1697e6a9102e024b80287d1
Ramakrishnan A <aramkris@gmail.com>
2019-01-24 16:33:14 +0530

    Set the Content-Type

--- main.go
+++ main.go
@@ -6,11 +6,11 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>First print</h1>")
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>Welcome to LocktheFrame</h1>")
 }
 
 func main() {
-	//fmt.Println("Hello! Lock the Frame")
 	http.HandleFunc("/", handlerFunc)
 	http.ListenAndServe("localhost:8000", nil)
 }
