e16ac771cf13c0d49ac552e398e46af910cc65bf
Ramakrishnan A <aramkris@gmail.com>
2019-01-24 17:15:26 +0530

    Added the '404' page

--- main.go
+++ main.go
@@ -12,6 +12,9 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "<h1>Welcome to LocktheFrame</h1>")
 	} else if r.URL.Path == "/contact" {
 		fmt.Fprint(w, "Please get in touch with us @ <a href=\"mailto:support@locktheframe.com\">support@locktheframe.com</a>")
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(w, "<h1>Sorry. We cannot find the page you are looking for. </h1> If you keep getting this error repeatedly, please email us @ <a href=\"mailto:support@locktheframe.com\">support@locktheframe.com</a>")
 	}
 }
 
