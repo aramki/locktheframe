a15947bb8c3348aa616396f94239515a45d084f4
Ramakrishnan A <aramkris@gmail.com>
2019-01-29 22:23:13 +0530

    Basic Gorilla Mux implementation

--- main.go
+++ main.go
@@ -2,23 +2,29 @@ package main
 
 import (
 	"fmt"
+	"github.com/gorilla/mux"
 	"net/http"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>Welcome to LocktheFrame</h1>")
+}
+
+func contact(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 
-	if r.URL.Path == "/" {
-		fmt.Fprint(w, "<h1>Welcome to LocktheFrame</h1>")
-	} else if r.URL.Path == "/contact" {
-		fmt.Fprint(w, "Please get in touch with us @ <a href=\"mailto:support@locktheframe.com\">support@locktheframe.com</a>")
-	} else {
-		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprint(w, "<h1>Sorry. We cannot find the page you are looking for. </h1> If you keep getting this error repeatedly, please email us @ <a href=\"mailto:support@locktheframe.com\">support@locktheframe.com</a>")
-	}
+	fmt.Fprint(w, "Please get in touch with us @ <a href=\"mailto:support@locktheframe.com\">support@locktheframe.com</a>")
 }
 
 func main() {
-	http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe("localhost:8000", nil)
+	//Initial Implementation
+	//http.HandleFunc("/", handlerFunc)
+	//http.ListenAndServe("localhost:8000", nil)
+
+	//Using Gorilla Mux
+	router := mux.NewRouter()
+	router.HandleFunc("/", home)
+	router.HandleFunc("/contact", contact)
+	http.ListenAndServe(":8000", router)
 }
