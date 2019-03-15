6a8b2c8f79ccb22f27f41051727f394a0d430d6c
Ramakrishnan A <aramkris@gmail.com>
2019-03-15 10:10:34 +0530

    Added a template file.

--- main.go
+++ main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"fmt"
 	"github.com/gorilla/mux"
+	"html/template"
 	"net/http"
+	"os"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>Welcome to LocktheFrame</h1>")
+	fmt.Fprint(w, "<h1>Welcome to Lock the Frame</h1>")
 }
 
 func contact(w http.ResponseWriter, r *http.Request) {
@@ -34,6 +36,31 @@ func main() {
 	//Initial Implementation
 	//http.HandleFunc("/", handlerFunc)
 	//http.ListenAndServe("localhost:8000", nil)
+
+	/*Using Templates*/
+	type User struct {
+		Name string
+		Dog  string
+		Num  int
+	}
+
+	templ, err := template.ParseFiles("./exp/hello.gohtml")
+	if err != nil {
+		panic(err)
+	}
+
+	data := User{
+		Name: "Maverick",
+		Dog:  "Labrador",
+		Num:  3,
+	}
+
+	err = templ.Execute(os.Stdout, data)
+	if err != nil {
+		panic(err)
+	}
+
+	/*End of Templates*/
 
 	//Using Gorilla Mux
 	var notFoundHandler http.Handler = http.HandlerFunc(handle404NotFound)
